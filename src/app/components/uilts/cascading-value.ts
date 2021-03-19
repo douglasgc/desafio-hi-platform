a4748fa86416e6b03c1a6018d906fa56cc617687
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2021-03-19 16:58:55 -0300

    fix(utils): utils folder fix name

--- src/app/components/uilts/cascading-value.ts
+++
@@ -1,9 +0,0 @@
-import { FormGroup } from '@angular/forms';
-
-export const cascadingFormGroup = (value: boolean, formGroup: FormGroup, ) => {
-    Object.keys(formGroup.controls)
-        .map((key) => formGroup.get(key))
-        .forEach((control) => {
-            control.get('value').setValue(value);
-        });
-};
