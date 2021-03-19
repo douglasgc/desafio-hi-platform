2caa97e460591a12a01fbc8f78432f3d49f2e871
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2021-03-19 15:20:27 -0300

    ci(init): add puppeteer for run chrome in karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -25,13 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: false,
     restartOnFileChange: true,
+    browsers: ['Chrome'],
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: "ChromeHeadless",
-        flags: ["--no-sandbox"]
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
       }
     },
   });
